737bbd98d2eff4276151a76ffcdb27477abdc50b
gyok <gyok@ya.ru>
2019-10-12 18:28:21 +0300

    first version of game

--- guessthenumberwindow.cpp
+++ guessthenumberwindow.cpp
@@ -1,6 +1,35 @@
 #include "guessthenumberwindow.h"
+#include <cstdlib>
+#include <ctime>
 
-GuessTheNumberWindow::GuessTheNumberWindow(QWidget *parent)
-    : QWidget(parent)
+GuessTheNumberWindow::GuessTheNumberWindow(QWidget *ip_parent)
+    : QWidget(ip_parent)
+    , m_layout{new QHBoxLayout(ip_parent)}
+    , mp_output{new QLabel(QString("Guess the number"), ip_parent)}
+    , mp_input{new QLineEdit(ip_parent)}
+    , mp_button{new QPushButton(QString("O&k"),ip_parent)}
 {
+    m_layout->addWidget(mp_output);
+    m_layout->addWidget(mp_input);
+    m_layout->addWidget(mp_button);
+    this->setLayout(m_layout);
+
+    connect(mp_button, &QPushButton::clicked, this, &GuessTheNumberWindow::CheckNumber);
+}
+
+int GuessTheNumberWindow::GenerateNumber()
+{
+    std::srand(unsigned(std::time(nullptr)));
+    return std::rand() % 3 + 1;
+}
+
+void GuessTheNumberWindow::CheckNumber()
+{
+    int num = GenerateNumber();
+    if (num == mp_input->text().toInt())
+    {
+        QMessageBox::information(this, "", "good");
+    } else {
+        QMessageBox::information(this, "", "bad");
+    }
 }
--- guessthenumberwindow.h
+++ guessthenumberwindow.h
@@ -1,5 +1,11 @@
 #pragma once
 #include <QWidget>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QLayout>
+#include <QHBoxLayout>
+#include <QMessageBox>
 
 class GuessTheNumberWindow : public QWidget
 {
@@ -7,4 +13,11 @@ class GuessTheNumberWindow : public QWidget
 
 public:
     GuessTheNumberWindow(QWidget *ip_parent = Q_NULLPTR);
+    QHBoxLayout* m_layout;
+    QLabel* mp_output;
+    QLineEdit* mp_input;
+    QPushButton* mp_button;
+private:
+    int GenerateNumber();
+    void CheckNumber();
 };
